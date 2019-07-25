ce4389be98a5ca8f5c5e164171ca95064aea5eb6
mhmmtkkl <makoklu32@gmail.com>
2019-07-24 20:57:03 -0400

    'Project2 '

---
+++ src/Methods.java
@@ -0,0 +1,26 @@
+
+public class Methods {
+
+	/*
+	 * First Method(RandomGenerator): Create a method public return type int , it should have 1 parameter max of random num
+	 * 
+	 * Second Method(TotalNum): Create a method public return type int , it should call the RandomGenerator Method two times sum of 2 numbers 
+	 * 
+	 * Third Method:Create a method public return type int , it should call TotalNum once and will have one parameter totalNum MINUS parameter
+	 * 
+	 * 
+	 * IF result of third method is less then 0 
+	 * 
+	 * syso( "Your total inserted amount is ="  .... +  )
+	 * 
+	 * first method Create a random number ===  Method Name 
+	 * 
+	 * second method Should get two random number from RandomGenerator method and will sum these two numbers 
+	 * 
+	 * 
+	 * third method sum of the numbers and will minus it with 12 and will display the result 
+	 * 
+	 * 
+	 */
+	
+}
